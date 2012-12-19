382da37191b5db9e0ff8a3bc18da3930ac1fc3d0
Stefan Reinhard <sr@feinheit.ch>
2012-12-19 01:58:27 +0100

    before shit gets wierd: refactor urls according to bdfls advice

--- urls.py
+++ urls.py
@@ -2,30 +2,60 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('keetab_cp.payments.views',
-    url(r'^$', 'index',
-        name='payment_index'),
+    url(
+        regex=r'^$',
+        view='index',
+        name='payment_index'
+    ),
 
-    url(r'^card/add/$', 'add_credit_card',
-        name='payment_add_credit_card'),
-    url(r'^card/confirm/$', 'confirm_credit_card',
-        name='payment_confirm_credit_card'),
+    url(
+        regex=r'^card/add/$',
+        view='add_credit_card',
+        name='payment_add_credit_card'
+    ),
+    url(
+        regex=r'^card/confirm/$',
+        view='confirm_credit_card',
+        name='payment_confirm_credit_card'
+    ),
 
-    url(r'^subscribe/(?P<plan_id>\w+)/', 'subscribe',
-        name="payment_subscribe"),
-    url(r'^unsubscribe/(?P<subscription_id>\w+)/$', 'unsubscribe',
-        name='payment_unsubscribe'),
+    url(
+        regex=r'^subscribe/(?P<plan_id>\w+)/',
+        view='subscribe',
+        name="payment_subscribe"
+    ),
+    url(
+        regex=r'^unsubscribe/(?P<subscription_id>\w+)/$',
+        view='unsubscribe',
+        name='payment_unsubscribe'
+    ),
 
-    url(r'^multiplesubcriptions/$', 'multiple_subscriptions',
-        name='payment_multiple_subscriptions'),
+    url(
+        regex=r'^multiplesubcriptions/$',
+        view='multiple_subscriptions',
+        name='payment_multiple_subscriptions'
+    ),
 
-    url(r'^change_to/(?P<plan_id>\w+)/$', 'change_to_plan',
-        name='payment_change_to_plan'),
-    url(r'^downgrade/free/$', 'downgrade_to_free_plan',
-        name='payment_downgrade_to_free_plan'),
+    url(
+        regex=r'^change_to/(?P<plan_id>\w+)/$',
+        view='change_to_plan',
+        name='payment_change_to_plan'
+    ),
+    url(
+        regex=r'^downgrade/free/$',
+        view='downgrade_to_free_plan',
+        name='payment_downgrade_to_free_plan'
+    ),
 
+    url(
+        regex=r'^webhook/$',
+        view='webhook',
+        name='payment_webhook'
+    ),
 
-    url(r'^webhook/$', 'webhook',
-        name='payment_webhook'),
-
-    url(r'^error/$', 'error', name='payment_error'),
+    url(
+        regex=r'^error/$',
+        view='error',
+        name='payment_error'
+    ),
 )
